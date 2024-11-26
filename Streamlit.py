79182b766a208d402699c5b0843520dafea38345
multiful <129200984+multiful@users.noreply.github.com>
2024-11-26 12:01:40 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import folium
 import pandas as pd
-from streamlit.components.v1 import iframe
-import pandas as pd
-import openpyxl
-import pandas as pd
+from streamlit_folium import st_folium  # streamlit-folium 라이브러리 사용
 import geopandas as gpd
-import json
 
 st.title('지도 시각화')
 
-#텍스트
+# 텍스트
 st.header('행정구역별 출생률 지도 시각화')
 df_br = pd.read_excel('assignment_3_df.xlsx')
 
@@ -18,10 +14,6 @@ st.write(df_br.head())
 
 # GeoDataFrame 로딩
 gdf_SGG = gpd.read_file('TL_SCCO_SIG.json')
-
-
-# 변환된 GeoDataFrame 출력
-st.write(gdf_SGG[['SIG_KOR_NM', 'geometry_wkt']].head())
 
 # 처리 함수 정의
 def process_location(data):
@@ -56,31 +48,32 @@ gdf_SGG_filtered['행정구역'] = gdf_SGG['SIG_KOR_NM'].apply(apply_processing)
 
 # 결과 확인
 st.write(gdf_SGG_filtered.head())
+
 # 한국 시각화
 
 title = '한국 시군구 별 출산율'
-title_gtml = title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
+title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
 
-korea = [35.907757, 127.766922] # 대한민국
+korea = [35.907757, 127.766922]  # 대한민국
 
 korea_map = folium.Map(
-    location = korea,
+    location=korea,
     zoom_start=7,
-    tiles = 'cartodbpositron'
+    tiles='cartodbpositron'
 )
 korea_map.get_root().html.add_child(folium.Element(title_html))
 
-# korea_map.save('data/korea_map.html') # 지도 저장
-
 # Choropleth map 그리기
-
-folium.Choropleth( 
-    geo_data = gdf_SGG_filtered,
-    data = df_br,
-    columns= ('행정구역별','합계출산율'),
-    key_on='feature.properties.행정구역', # key
-    fill_color = 'BuPu',
-    fill_opacity = 0.7,
-    line_opacity = 0.5,
-    legend_name = '출산율'
+folium.Choropleth(
+    geo_data=gdf_SGG_filtered,
+    data=df_br,
+    columns=('행정구역별', '합계출산율'),
+    key_on='feature.properties.행정구역',  # key
+    fill_color='BuPu',
+    fill_opacity=0.7,
+    line_opacity=0.5,
+    legend_name='출산율'
 ).add_to(korea_map)
+
+# Streamlit에서 folium 지도 표시
+st_folium(korea_map, width=700, height=500)
